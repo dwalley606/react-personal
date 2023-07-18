31d9f343876449c960445338487d130516bc595a
dwalley606 <dwalley606@gmail.com>
2023-07-18 19:49:42 -0400

    Added empty components and set up a router

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -3,17 +3,24 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function TopBar() {
   return (
+    <Router>
     <Navbar data-bs-theme="dark" expand="lg" className="bg-body-tertiary">
       <Container>
         <Navbar.Brand href="#home">Daniel Walley</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link as={Link} to={"/home"}>Home</Nav.Link>
+            <Nav.Link as={Link} to={"/about"}>About</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
@@ -29,6 +36,7 @@ function TopBar() {
         </Navbar.Collapse>
       </Container>
     </Navbar>
+    </Router>
   );
 }
 
